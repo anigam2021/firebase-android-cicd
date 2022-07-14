d7bc053cbdf5340a02edbcc1c62c91a92a08fb01
apple <apple@apples-MacBook-Pro-2.local>
2022-07-14 13:09:37 +0530

    develop

--- .github/scripts/set-env-from-xcodeproj.sh
+++ .github/scripts/set-env-from-xcodeproj.sh
@@ -3,5 +3,5 @@
 set -euo pipefail
 
 SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
-PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = /ios/")"
+PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = /ios")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
