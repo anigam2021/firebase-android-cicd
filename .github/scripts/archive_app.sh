e044d0bd29651daeb28fb8b903b3dcae9334a855
apple <apple@apples-MacBook-Pro-2.local>
2022-07-14 14:12:31 +0530

    develop

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 xcodebuild -workspace ios/eatch.xcworkspace \
-            -scheme ios \
+            -scheme Eatch \
             -sdk iphoneos \
             -configuration AppStoreDistribution \
             -archivePath $PWD/build/eatch.xcarchive \
