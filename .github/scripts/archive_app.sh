34767a6fbffeb1c2772452bc67e85a0cf1e4166c
apple <apple@apples-MacBook-Pro-2.local>
2022-07-21 18:26:41 +0530

    chnages

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 
 #xcodebuild -workspace ios/eatch.xcworkspace -scheme eatch -sdk iphoneos  --configuration archive -archivePath $PWD/build/eatch.xcarchive -allowProvisioningUpdates
 
-xcodebuild -workspace ios/eatch.xcworkspace -scheme eatch -destination 'generic/platform=iOS' -archivePath $PWD/build/eatch.xcarchive -allowProvisioningUpdates
+xcodebuild -workspace ios/eatch.xcworkspace -scheme eatch -destination 'generic/platform=iOS' -archivePath $PWD/build/eatch.xcarchive 
